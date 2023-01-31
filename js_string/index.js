755be7d8bfcc0d87f6eb5faa83698aa74a87efff
Michael Schultz <mikewschultz9029@gmail.com>
2023-01-31 16:11:56 -0500

    added js_string subdirectory

---
+++ js_string/index.js
@@ -0,0 +1,11 @@
+let firstName = 'Mike'
+let lastName = 'Schultz'
+
+console.log('First: ' + firstName, 'Last: ' + lastName)
+
+let fullName = firstName + ' ' + lastName
+
+console.log('%cFull name: ' + fullName, 'color: orange;')
+
+let result = prompt("What's your name?")
+console.log('Hello ' + result)
