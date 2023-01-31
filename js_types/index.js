d8b0013587c4eba1d3e619a372ab706946f7b5e2
Michael Schultz <mikewschultz9029@gmail.com>
2023-01-31 15:09:01 -0500

    added styling and event listeners

--- js_types/index.js
+++ js_types/index.js
@@ -1,3 +1,22 @@
-let h2 = document.createElement('h2')
-h2.innerText = 'Hello World'
-document.querySelector('body').appendChild(h2)
+let i = 0
+
+document.addEventListener('click', addGreeting)
+document.querySelector('h1').addEventListener('mouseover', colorShift)
+
+function addGreeting()
+{
+    let h2 = document.createElement('h2')
+    h2.innerText = 'Hello World'
+    document.querySelector('body').appendChild(h2)
+}
+
+function colorShift(e)
+{
+    const COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    // console.log(e.target)
+    e.target.style = `color: ${COLORS[i]};`
+    ++i
+    // to make sure iterator doesn't move past end of COLORS
+    if (i >= COLORS.length)
+        i = 0
+}
