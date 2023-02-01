1b5191b97b99221d10f6c8e543e84d8656056fe5
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-01 13:24:30 -0500

    checked out math operations and library

---
+++ js_math/index.js
@@ -0,0 +1,40 @@
+let num1 = 100, num2 = 50, num3 = 13
+let value
+
+value = num1 + num2
+
+console.log(value)
+
+value = num1 * num2
+console.log(value)
+
+value = num1 - num2
+console.log(value)
+
+value = num1 / num2
+console.log(value)
+
+value = num1 % num3
+console.log(value)
+
+value = num1 % 2
+console.log(value)
+
+value = num3 % 2
+console.log(value)
+
+value = 4 ** 4
+console.log(value)
+
+value = Math.pow(num3, 2)
+console.log(value)
+
+value = Math.sqrt(69)
+console.log(value)
+
+value = Math.sqrt(144)
+console.log(value)
+
+// 1 - 100
+value = Math.ceil(Math.random() * 100)
+console.log(value)
