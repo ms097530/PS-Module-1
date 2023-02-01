ffce7c7c52503e5b722fbb95a52163c3cc7d8d92
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-01 09:50:15 -0500

    chat log not displaying properly

---
+++ chatbot/index.js
@@ -0,0 +1,38 @@
+let namePrompt = 'Hello, what is your name?'
+let username = prompt(namePrompt)
+
+let currContent = document.createElement('p')
+
+// not working while going between prompts and alerts
+currContent.innerText = namePrompt
+document.body.appendChild(currContent)
+currContent.innerText = username
+document.body.appendChild(currContent)
+
+// manipulations to DOM only seem to appear at end of sequence (so currContext paragraph is only added once at the end)
+let testItem = document.createElement('h2')
+testItem.innerText = 'DUMMY TEXT'
+document.body.appendChild(testItem)
+
+let moodPrompt = `Nice to meet you, ${username}! How are you?`
+let mood = prompt(moodPrompt)
+
+// works between prompts and alerts
+// console.log('TEST')
+
+// not working while going between prompts and alerts
+currContent.innerText = 'TEST'
+document.body.appendChild(currContent)
+
+// not working while going between prompts and alerts
+currContent.innerText = moodPrompt
+document.body.appendChild(currContent)
+currContent.innerText = mood
+document.body.appendChild(currContent)
+
+alert(`Great! I'm glad to hear you're ${mood}! (unless that's not good...)`)
+
+
+
+// currContent.innerText = 'TEST'
+// document.body.appendChild(currContent)
