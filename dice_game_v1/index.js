c7885208243b83954cec895a1e6011bb42ef5c24
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-08 11:51:17 -0500

    added comments and styling to logs

--- dice_game_v1/index.js
+++ dice_game_v1/index.js
@@ -5,20 +5,22 @@
 
 function rollDice(p1_Points = 0, computer_Points = 0, turnNumber = 1)
 {
+    // base conditions - winner is decided
     if (p1_Points >= 5)
     {
-        console.log('YOU WIN')
+        console.log('%cYOU WIN!', 'color: limegreen; font-size: 2rem;')
         return
     }
     if (computer_Points >= 5)
     {
-        console.log('You lose...')
+        console.log('%cYou lose...', 'color: rgb(255, 70, 30); font-size: 1.5rem;')
         return
     }
 
+    // roll 6-sided dies
     let playerRoll = Math.ceil(Math.random() * 6)
     let computerRoll = Math.ceil(Math.random() * 6)
-
+    // get round results
     let isTie = playerRoll === computerRoll
     let isPlayerWin = playerRoll > computerRoll
 
@@ -26,9 +28,7 @@ function rollDice(p1_Points = 0, computer_Points = 0, turnNumber = 1)
     console.log(`You rolled ${playerRoll}`)
     console.log(`Your opponent rolled ${computerRoll}`)
 
-
-
-
+    // advance to next round
     if (isTie)
     {
         printScores(isTie, isPlayerWin)
@@ -45,6 +45,7 @@ function rollDice(p1_Points = 0, computer_Points = 0, turnNumber = 1)
         return rollDice(p1_Points, computer_Points + 1, turnNumber + 1)
     }
 
+    // helper function for printing round results
     function printScores(isTie, isPlayerWinner)
     {
         if (isTie)
@@ -63,18 +64,6 @@ function rollDice(p1_Points = 0, computer_Points = 0, turnNumber = 1)
             console.log(`%cComputer score: ${computer_Points + 1}`, 'color: pink')
         }
     }
-    // if (playerRoll > computerRoll)
-    // {
-    //     return rollDice(p1_Points + 1, computer_Points, turnNumber + 1)
-    // }
-    // else if (computerRoll > playerRoll)
-    // {
-    //     return rollDice(p1_Points, computer_Points + 1, turnNumber + 1)
-    // }
-    // else
-    // {
-    //     return rollDice(p1_Points, computer_Points, turnNumber + 1)
-    // }
 }
 
 rollDice()
