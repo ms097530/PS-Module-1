433206e91d670b948bf15dd0a434cdc47d62634a
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-08 11:34:20 -0500

    added factorial function

--- js_functions/recursion.js
+++ js_functions/recursion.js
@@ -20,3 +20,12 @@ function sumR(num, n)
 
     return sumR(num + 2, n - 1)
 }
+
+function factorial(n)
+{
+    if (n <= 0)
+        return 1
+
+    console.log(`Will return ${n} * factorial(${n - 1})`)
+    return n * factorial(n - 1)
+}
