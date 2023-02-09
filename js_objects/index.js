036e306bec43b53256c53a3bf2d8c78a61e18e43
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-09 13:09:45 -0500

    added "this" demo

--- js_objects/index.js
+++ js_objects/index.js
@@ -35,3 +35,64 @@ const pet = {
 }
 
 console.log(pet)
+
+// Week 3 / day 3
+
+const adventurer = {
+    name: 'Kiryu',
+    title: 'Dragon of Dojima',
+    hitPoints: 999,
+    styles: ['brawler', 'beast', 'rush', 'dragon'],
+    weapons: ['fists', 'marlin cannon', 'taser', 'sakura storm', 'kali sticks'],
+    majima: {
+        foundKiryu: true,
+        versions: ['cop', 'construction worker', 'breakdancer', 'thug']
+    }
+}
+
+for (let prop in adventurer)
+{
+    console.log(prop)
+}
+
+for (const [key, val] of Object.entries(adventurer))
+{
+    console.log(`key: ${key}   val: ${val}`)
+}
+
+adventurer.sayHi = () => { console.log('Yo.') }
+adventurer.sayHi()
+
+adventurer.companion = 'Rikiya'
+console.log('Kiryu\'s buddy: ' + adventurer.companion)
+// ... dies
+delete adventurer.companion
+console.log(adventurer.companion)
+
+// Array of objects
+
+const anime = [
+    { title: 'Mob Psycho', numSeasons: 3 },
+    { title: 'Clannad', numSeasons: 2 },
+    { title: 'Gurren Lagann', numSeasons: 1 },
+    { title: 'Attack on Titan', numSeasons: 5 },
+    { title: 'Bocchi the Rock', numSeasons: 1 },
+    { title: 'Spy x Family', numSeasons: 2 }
+]
+
+
+const player = {
+    name: 'Dart',
+    stats: {
+        hp: 879,
+        atk: 142,
+        mp: 40
+    },
+    catchphrase: 'Lavitz is my bro',
+    sayCatchphrase: function ()
+    {
+        console.log(`${this.name}: ${this.catchphrase}`)
+    }
+}
+
+player.sayCatchphrase()
