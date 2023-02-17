ef05ef4213d5d9b24e6fab1a902c04705e87b075
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-17 12:07:28 -0500

    added insertByIndex to LinkedList

--- data_structures/data_structures.js
+++ data_structures/data_structures.js
@@ -7,3 +7,52 @@ export class Node
         this.next = null
     }
 }
+
+// LinkedList class
+export class LinkedList
+{
+    constructor()
+    {
+        this.head = null
+    }
+    getHeadNode()
+    {
+        return this.head
+    }
+    clear()
+    {
+        this.head = null
+    }
+
+    traverse()
+    {
+        // first Node in the list
+        let walker = this.head
+        while (walker)
+        {
+            // print current node's data value
+            console.log('This node\'s data value is: ' + walker.data)
+            // move walker to next Node
+            walker = walker.next
+        }
+    }
+
+    insertByIndex(index, val)
+    {
+        let i = 0
+        let walker = this.head
+        while (i < index && walker.next)
+        {
+            ++i
+            walker = walker.next
+        }
+        // console.dir('Walker: ')
+        // console.dir(walker)
+        // console.log('walker next: ')
+        // console.log(walker.next)
+
+        let newNode = new Node(val)
+        newNode.next = walker.next
+        walker.next = newNode
+    }
+}
--- data_structures/index.js
+++ data_structures/index.js
@@ -1,4 +1,27 @@
-import { Node } from "./data_structures.js";
+import { Node, LinkedList } from "./data_structures.js";
 
-let dummy = new Node('test')
-console.log(dummy)
+const node1 = new Node(4)
+const node2 = new Node(7)
+const node3 = new Node(12)
+
+const myLinkedList = new LinkedList()
+
+// head: 4 -> 7 -> 12 -> null
+
+myLinkedList.head = node1
+
+console.log(myLinkedList)
+myLinkedList.head.next = node2
+myLinkedList.head.next.next = node3
+// alternatively
+// node1.next = node2
+// node2.next = node3
+
+console.log(myLinkedList)
+
+// console.log(myLinkedList.getHeadNode())
+
+myLinkedList.traverse()
+
+myLinkedList.insertByIndex(1, 69)
+console.log(myLinkedList)
