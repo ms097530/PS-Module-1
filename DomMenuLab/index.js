999f7dd802359a7e9d34829c05b20bd1eb5e0f16
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-21 13:52:52 -0500

    completed tasks for DOM menu lab part 1

---
+++ DomMenuLab/index.js
@@ -0,0 +1,30 @@
+// Tasks 1.0 - 1.3
+let mainEl = document.querySelector('main')
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+mainEl.classList.add('flex-ctr')
+
+// Tasks 2.0 - 2.3
+let topMenuEl = document.querySelector('#top-menu')
+
+topMenuEl.style.height = '100%'
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg'
+topMenuEl.classList.add('flex-around')
+
+// Tasks 3.0 - 3.1
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
+menuLinks.forEach(link =>
+{
+    let anchor = document.createElement('a')
+    anchor.setAttribute('href', link.href)
+    anchor.innerText = link.text
+    topMenuEl.appendChild(anchor)
+})
