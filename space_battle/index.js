3bc254080b30347ba88297f9372be6c11e9df100
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-24 11:14:01 -0500

    created prompt loop inside startShipBattle function

--- space_battle/index.js
+++ space_battle/index.js
@@ -7,9 +7,15 @@ class Ship
         this.accuracy = accuracy
     }
 
-    attack()
+    attack(enemyShip)
     {
-
+        let randomNum = Math.random()
+        if (randomNum <= this.accuracy)
+        {
+            enemyShip.hull -= this.firepower
+            return true
+        }
+        return false
     }
 
     retreat()
@@ -28,18 +34,32 @@ const ENEMY_VALUES = {
     firepower: { min: 2, max: 4, round: true },
     accuracy: { min: 0.6, max: 0.8, round: false }
 }
-console.log(ENEMY_VALUES)
-let enemyHull = Math.random()
-let enemyShip = new Ship()
 
-generateShip(ENEMY_VALUES)
+
+let enemyShip = generateShip(ENEMY_VALUES)
+console.log(enemyShip)
+
+startShipBattle(yourShip, enemyShip)
+
+function startShipBattle(shipOne, shipTwo)
+{
+    // shipOne goes first
+    let command
+    while (command !== '1' && command !== '2')
+    {
+        command = window.prompt(`Current HP: ${shipOne.hull}  Enemy HP: ${shipTwo.hull}\n1: Attack  2: Retreat`)
+        console.log(command)
+        console.log(typeof command)
+    }
+    console.log('outside loop')
+}
 
 
 function generateShip(valuesObj)
 {
     // loop through any number of desired parameters and generate values
     let parameters = []
-    for (let [key, val] of Object.entries(valuesObj))
+    for (let val of Object.values(valuesObj))
     {
         // console.log('val: ', val)
         let param
@@ -54,7 +74,7 @@ function generateShip(valuesObj)
         }
         parameters.push(param)
     }
-    console.log(parameters)
+    // console.log(parameters)
 
     // one-by-one with specific names - have to add new parameters here each time a new parameter is desired
     // let hull = Math.round(Math.random() * (valuesObj.hull.max - valuesObj.hull.min) + valuesObj.hull.min)
@@ -68,7 +88,7 @@ function generateShip(valuesObj)
 
     // produce new ship with generated parameters
     let newShip = new Ship(...parameters)
-    console.log(newShip)
+    // console.log(newShip)
     return newShip
 }
 
