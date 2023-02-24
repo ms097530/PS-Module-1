4e6f92b519e7d470caf867a9eb4b7b9c775ecca9
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-24 10:29:31 -0500

    finished generateShip function - version 1

--- space_battle/index.js
+++ space_battle/index.js
@@ -33,22 +33,43 @@ let enemyHull = Math.random()
 let enemyShip = new Ship()
 
 generateShip(ENEMY_VALUES)
+
+
 function generateShip(valuesObj)
 {
+    // loop through any number of desired parameters and generate values
     let parameters = []
     for (let [key, val] of Object.entries(valuesObj))
     {
-        console.log('key: ', key)
-        console.log('val: ', val)
+        // console.log('val: ', val)
+        let param
+        if (val.round)
+        {
+            param = Math.round(Math.random() * (val.max - val.min) + val.min)
+
+        }
+        else
+        {
+            param = Math.random() * (val.max - val.min) + val.min
+        }
+        parameters.push(param)
     }
-    let hull = Math.round(Math.random() * (valuesObj.hull.max - valuesObj.hull.min) + valuesObj.hull.min)
-    console.log(hull)
-    let firepower = Math.round(Math.random() * (valuesObj.firepower.max - valuesObj.firepower.min) + valuesObj.firepower.min)
-    console.log(firepower)
-    let accuracy = Math.random() * (valuesObj.accuracy.max - valuesObj.accuracy.min) + valuesObj.accuracy.min
-    console.log(accuracy)
+    console.log(parameters)
+
+    // one-by-one with specific names - have to add new parameters here each time a new parameter is desired
+    // let hull = Math.round(Math.random() * (valuesObj.hull.max - valuesObj.hull.min) + valuesObj.hull.min)
+    // console.log(hull)
+    // let firepower = Math.round(Math.random() * (valuesObj.firepower.max - valuesObj.firepower.min) + valuesObj.firepower.min)
+    // console.log(firepower)
+    // let accuracy = Math.random() * (valuesObj.accuracy.max - valuesObj.accuracy.min) + valuesObj.accuracy.min
+    // console.log(accuracy)
     // let newShip = new Ship(hull, firepower, accuracy)
     // return newShip
+
+    // produce new ship with generated parameters
+    let newShip = new Ship(...parameters)
+    console.log(newShip)
+    return newShip
 }
 
 // while (!isGameOver)
